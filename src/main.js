ba3418df422bdff1f567ba3d657e5c977b43af24
Junk-debug <maks.naumovich@gmail.com>
2024-12-18 17:18:21 +0100

    feat: add initial three.js project structure with HTML, CSS, and Webpack configuration

--- src/main.js
+++ src/main.js
@@ -1,29 +1,30 @@
 import * as THREE from "three";
 
+// Сцена
 const scene = new THREE.Scene();
 
+// Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: "purple",
-});
+const material = new THREE.MeshBasicMaterial({ color: "purple" });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 
+// Камера
 const sizes = {
   width: 600,
   height: 600,
 };
-
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
+camera.position.z = 3;
+camera.position.y = 1;
 
-mesh.rotateOnAxis(new THREE.Vector3(1, 1, 1), 0.5);
+scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+const canvas = document.querySelector(".canvas");
 
-const canvas = renderer.domElement;
-document.body.appendChild(canvas);
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(sizes.width, sizes.height);
 
 renderer.render(scene, camera);
