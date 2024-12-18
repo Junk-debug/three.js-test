b67108a206b57afa90e0e7b4321608415812d469
Junk-debug <maks.naumovich@gmail.com>
2024-12-18 17:39:15 +0100

    feat: enhance 3D scene with axes helper and modify mesh properties

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import * as THREE from "three";
 // Сцена
 const scene = new THREE.Scene();
 
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
 // Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const material = new THREE.MeshBasicMaterial({
+  color: "purple",
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
@@ -17,13 +23,17 @@ const sizes = {
 };
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.y = 1;
+// camera.position.y = 1;
 
 scene.add(camera);
 
 const canvas = document.querySelector(".canvas");
 
 const renderer = new THREE.WebGLRenderer({ canvas });
+
+mesh.scale.x = 0.5;
+mesh.scale.y = 2;
+mesh.scale.z = -0.7;
 
 renderer.setSize(sizes.width, sizes.height);
 
